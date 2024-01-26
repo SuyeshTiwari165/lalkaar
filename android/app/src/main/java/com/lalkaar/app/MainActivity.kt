6a1db9909023f1cdddeb0420f0f66197a588711a
Suyesh Tiwari <suyeshtiwari123@gmail.com>
2024-01-26 15:57:56 +0530

    Add Changes

--- android/app/src/main/java/com/lalkaar/app/MainActivity.kt
+++ android/app/src/main/java/com/lalkaar/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.lalkaar
+package com.lalkaar.app
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/lalkaar/app/MainApplication.kt
+++ android/app/src/main/java/com/lalkaar/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.lalkaar
+package com.lalkaar.app
 
 import android.app.Application
 import android.content.res.Configuration
